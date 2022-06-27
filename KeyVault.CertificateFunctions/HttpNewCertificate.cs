4f8d905c7bcc8a6234bc2dd422170d1b6939b5f8
Raphael Bickel <raphael.bickel@gmail.com>
2022-06-27 14:44:27 +0200

    Change param name add valiation

--- KeyVault.CertificateFunctions/HttpNewCertificate.cs
+++ KeyVault.CertificateFunctions/HttpNewCertificate.cs
@@ -9,31 +9,68 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using KeyVault.CertificateAuthority;
 using Azure.Identity;
+using System.Text.RegularExpressions;
 
 namespace KeyVault.TlsAutoRenew
 {
-    
+
     public static class HttpNewCertificate
     {
+
+        private static Regex _nameRegex = new Regex("^[a-zA-Z0-9-]+$");
+        //very basic SAN validation
+        private static Regex _subjectRegex = new Regex(@"^[a-zA-Z0-9-*\.]+$");
+
         [FunctionName("NewTlsCertificate")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string defaultKeyVaultUri= Environment.GetEnvironmentVariable("DefaultKeyVaultUri");
+            string defaultKeyVaultUri = Environment.GetEnvironmentVariable("DefaultKeyVaultUri");
             string defaultDurationDays = Environment.GetEnvironmentVariable("DefaultCertificateDuration");
             string defaultCA = Environment.GetEnvironmentVariable("DefaultKeyCACertificate");
 
-            log.LogInformation("C# HTTP trigger function processed a request.");    
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
             string subject = req.Query["subject"];
-            string[] fqdn = req.Query["fqdn"];
+            string[] san = req.Query["san"];
             string issuer = defaultCA;
+
+            //parameters validation
+            if (!_nameRegex.Match(name ?? string.Empty).Success)
+            {
+                // does not match
+                return new ObjectResult($"Invalid certificate name provided. Must match {_nameRegex}")
+                {
+                    StatusCode = 400
+                };
+            }
+
+            if (!_subjectRegex.Match(subject ?? string.Empty).Success)
+            {
+                // does not match
+                return new ObjectResult($"Invalid subject name provided. Must match {_subjectRegex}")
+                {
+                    StatusCode = 400
+                };
+            }
+
+            foreach (var s in san)
+            {
+                if (!_subjectRegex.Match(s ?? string.Empty).Success)
+                {
+                    // does not match
+                    return new ObjectResult($"Invalid SAN name provided. Must match {_subjectRegex}")
+                    {
+                        StatusCode = 400
+                    };
+                }
+            }
 
             //missing params validation
             var kvCertProvider = KeyVaultCertificateProvider.GetKeyVaultCertificateProvider(defaultKeyVaultUri, log);
-            await kvCertProvider.CreateCertificateAsync(issuer, name, $"CN={subject}",  int.Parse(defaultDurationDays), fqdn, 1);
+            await kvCertProvider.CreateCertificateAsync(issuer, name, $"CN={subject}", int.Parse(defaultDurationDays), san, 1);
 
             string responseMessage = "Certificate generated successfully";
             return new OkObjectResult(responseMessage);
