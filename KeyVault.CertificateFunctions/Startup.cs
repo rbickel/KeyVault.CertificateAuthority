4535116a7ab3d179cadb3d4e07cb97765dead2b0
Raphael Bickel <raphael.bickel@gmail.com>
2022-06-29 16:19:06 +0200

    Add swagger

---
+++ KeyVault.CertificateFunctions/Startup.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Reflection;
+using AzureFunctions.Extensions.Swashbuckle;
+using AzureFunctions.Extensions.Swashbuckle.Settings;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+[assembly: WebJobsStartup(typeof(KeyVault.CertificateFunctions.KeyVaultCertificateFunctionsStartup))]
+namespace KeyVault.CertificateFunctions
+{
+    public class KeyVaultCertificateFunctionsStartup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.AddSwashBuckle(Assembly.GetExecutingAssembly(), opts =>
+            {
+                opts.AddCodeParameter = true;
+                opts.Documents = new[] {
+                    new SwaggerDocument {
+                        Name = "v1",
+                            Title = "Swagger document",
+                            Description = "Integrate Swagger UI With Azure Functions",
+                            Version = "v2"
+                    }
+                };
+                opts.ConfigureSwaggerGen = x =>
+                {
+                    x.CustomOperationIds(apiDesc =>
+                    {
+                        return apiDesc.TryGetMethodInfo(out MethodInfo mInfo) ? mInfo.Name : default(Guid).ToString();
+                    });
+                };
+            });
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            base.ConfigureAppConfiguration(builder);
+        }
+    }
+}
